de0fc8cec94d4639d95002de69ec8eb9f281ec5c
Casaemiro <silasmagho@gmail.com>
2022-06-16 08:21:42 +0100

    questions

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
             <h1>A.Silas Magho</h1>
             <div className="links">
                 <a href="iknite.space">about me</a>
-                <a href="iknite.space">download_resume</a>
+                <a href="https://drive.google.com/file/d/10LFultyGOU8XO3I8oNF6AEJN_3fTz77Q/view"  download>download_resume</a>
                 <a href="#portfolio">portfolio</a>
                 <a href="iknite.space">help</a>
                 <a href="https://github.com/Casaemiro">GitHub</a>
