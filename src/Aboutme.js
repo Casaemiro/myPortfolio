f918e542a452eed0c566fcb2854365b102428bbc
Casaemiro <silasmagho@gmail.com>
2022-06-17 15:21:58 +0100

    responsive-version

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -1,58 +1,58 @@
 const Aboutme = () => {
-    return ( 
+    return (
         <>
             <div className="about">
-            <h2>Education</h2>
-            <div className="aboutText">
-                <h4>2010-2015</h4>
-                <span>(Seconday school) Ordinary level certificate in GBHS Kumbo</span>
+                <h2>Education</h2>
+                <div className="aboutText">
+                    <h4>2010-2015</h4>
+                    <span>(Seconday school) Ordinary level certificate in GBHS Kumbo</span>
+                </div>
+                <div className="aboutText">
+                    <h4>2015-2017</h4>
+                    <span>(High school) Advance level certificate in GBHS Kumbo</span>
+                </div>
+                <div className="aboutText">
+                    <h4>2017-2020</h4>
+                    <span>B.Sc in Computer Science in the university of Buea in Cameroon</span>
+                </div>
+                <div className="aboutText">
+                    <h4>2020-2022</h4>
+                    <span>M.tech in Computer Engineering in the university of Buea in Cameroon</span>
+                </div>
             </div>
-            <div className="aboutText">
-                <h4>2015-2017</h4>
-                <span>(High school) Advance level certificate in GBHS Kumbo</span>
+            <div className="about">
+                <h2>Certifications</h2>
+                <div className="aboutText">
+                    <h4>2022</h4>
+                    <span>Responsive web design (Free Code Camp)</span>
+                </div>
+                <div className="aboutText">
+                    <h4>2022</h4>
+                    <span>JavaScript Data Structures and Algoruthms (Free Code Camp)</span>
+                </div>
+                <div className="aboutText">
+                    <h4>2022</h4>
+                    <span>Front End Libraries (Free Code Camp)</span>
+                </div>
+                <div className="aboutText">
+                    <h4>2022</h4>
+                    <span>Introduction to Machine learning</span>
+                </div>
+                <div className="aboutText">
+                    <h4>2022</h4>
+                    <span>Python basics (Kaggle)</span>
+                </div>
             </div>
-            <div className="aboutText">
-                <h4>2017-2020</h4>
-                <span>B.Sc in Computer Science in the university of Buea in Cameroon</span>
+            <div className="about">
+                <h2>Work Experience</h2>
+                <div className="aboutText">
+                    <h4>2022</h4>
+                    <span>6 months Web/moble app develpement training and internship at iknitespace</span>
+                </div>
             </div>
-            <div className="aboutText">
-                <h4>2020-2022</h4>
-                <span>M.tech in Computer Engineering in the university of Buea in Cameroon</span>
-            </div>
-        </div>
-        <div className="about">
-            <h2>Certifications</h2>
-            <div className="aboutText">
-                <h4>2022</h4>
-                <span>Responsive web design (Free Code Camp)</span>
-            </div>
-            <div className="aboutText">
-                <h4>2022</h4>
-                <span>JavaScript Data Structures and Algoruthms (Free Code Camp)</span>
-            </div>
-            <div className="aboutText">
-                <h4>2022</h4>
-                <span>Front End Libraries (Free Code Camp)</span>
-            </div>
-            <div className="aboutText">
-                <h4>2022</h4>
-                <span>Introduction to Machine learning</span>
-            </div>
-            <div className="aboutText">
-                <h4>2022</h4>
-                <span>Python basics (Kaggle)</span>
-            </div>
-        </div>
-        <div className="about">
-            <h2>Work Experience</h2>
-            <div className="aboutText">
-                <h4>2022</h4>
-                <span>6 months Web/moble app develpement training and internship at iknitespace</span>
-            </div>
-        </div>
         </>
-        
-     );
+
+    );
 }
- 
+
 export default Aboutme;
